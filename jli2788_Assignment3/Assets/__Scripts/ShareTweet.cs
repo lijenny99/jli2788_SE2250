8e3bcc61a463d0e03d5e9dbeac9989c31fc904aa
Jenny Li <lijenny1999@gmail.com>
2019-03-18 17:09:29 -0400

    adding assignment 3

---
+++ jli2788_Assignment3/Assets/__Scripts/ShareTweet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShareTweet : MonoBehaviour
+{
+    private const string _twitterAddress = "https://twitter.com/intent/tweet";
+
+    public void ShareToTwitter()
+    {
+        if (ScoreManager.SCORE > ScoreManager.HIGH_SCORE)
+        {
+            Application.OpenURL(_twitterAddress + "?text=" + WWW.EscapeURL("Congratulations! You earned a high score of " + ScoreManager.SCORE));
+        }
+        else
+        {
+            Application.OpenURL(_twitterAddress + "?text=" + WWW.EscapeURL("Congratulations! You earned a score of " + ScoreManager.SCORE));
+        }
+    }
+}
